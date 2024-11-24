eb6ee27be992e946480456ab070bf6bab5449d69
Divyanshu Rawat <120498621+divyansshu@users.noreply.github.com>
2024-11-24 14:44:38 +0530

    symbol hunt game

---
+++ symbolHunt.py
@@ -0,0 +1,58 @@
+import random
+import time
+from tkinter import Tk, Button, DISABLED, messagebox
+
+def show_symbol(x, y):
+    global first
+    global previousx, previousy
+    global matches_found
+
+    buttons[x, y]['text'] = button_symbols[x, y]
+    buttons[x, y].update_idletasks()
+
+    if first:
+        previousx = x
+        previousy = y
+        first = False
+    elif previousx != x or previousy != y:
+        if buttons[previousx, previousy]['text'] != buttons[x, y]['text']:
+            time.sleep(0.5)
+            buttons[previousx, previousy]['text'] = ' '
+            buttons[x, y]['text'] = ' '
+        else:
+            buttons[previousx, previousy]['command'] = DISABLED
+            buttons[x, y]['command'] = DISABLED
+            matches_found += 1
+            if matches_found == total_matches:
+                messagebox.showinfo("Congratulations!", "You've successfully completed the game!")
+        first = True
+
+win = Tk()
+win.title('Matchmaker')
+win.resizable(width=False, height=False)
+
+first = True
+previousx = 0
+previousy = 0
+matches_found = 0
+total_matches = 12  # Total pairs in the game
+
+buttons = {}
+button_symbols = {}
+symbols = [
+    u'\u2702', u'\u2705', u'\u2708', u'\u2709', u'\u270A', u'\u270B',
+    u'\u270C', u'\u270F', u'\u2712', u'\u2714', u'\u2716', u'\u2728',
+    u'\u2702', u'\u2705', u'\u2708', u'\u2709', u'\u270A', u'\u270B',
+    u'\u270C', u'\u270F', u'\u2712', u'\u2714', u'\u2716', u'\u2728'
+]
+
+random.shuffle(symbols)
+
+for x in range(6):
+    for y in range(4):
+        button = Button(command=lambda x=x, y=y: show_symbol(x, y), width=10, height=8)
+        button.grid(column=x, row=y)
+        buttons[x, y] = button
+        button_symbols[x, y] = symbols.pop()
+
+win.mainloop()
